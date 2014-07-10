d0153aa93273650f4cc41c20197a9664490ece1c
Elias Martenson <lokedhs@gmail.com>
2014-07-10 18:51:54 +0800

    Add APL_DIST to dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 ((c++-mode . ((eval . (progn
                         (em-append-include-dirs (list (expand-file-name "~/src/apl/src")
+                                                      (expand-file-name "~/src/apl")
                                                       "/usr/include/postgresql"))
                         (flycheck-mode 1)
                         (company-mode 1))))))
